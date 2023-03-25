5a68d015d76e6f2f4af1974cb20d83c7ea713017
unknown <dollorinfotech@gmail.com>
2023-03-25 10:16:00 +0530

    new

--- src/pages/SearchFilter.js
+++ src/pages/SearchFilter.js
@@ -5,10 +5,13 @@ import { Badge, Button, Card, Col, ListGroup, Row } from 'react-bootstrap'
 import { Link, useSearchParams } from 'react-router-dom'
 import { URL } from '../helpers/helper'
 
+import img from '../assets/img/1.png';
+
 export default function SearchFilter() {
     //2.1 Hooks area
     const [businesses,setBusinesses] = useState([])
-    const [searchParams, setSearchParams] = useSearchParams()
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [star,setStar] = useState([])
 
     useEffect(()=>{
         console.log('cat_name-------->',searchParams.get('cat_name'));
@@ -16,8 +19,14 @@ export default function SearchFilter() {
         fetch(`${URL}/api/businesses?populate=*&filters[business_categories][name][$containsi]=${searchParams.get('cat_name')}`)
         .then(res=>res.json())
         .then(data=>{
-            console.log(data.data);
-            setBusinesses(data.data)
+            console.log('data.data -------->',data.data);
+            setBusinesses(data.data);
+            for (let index = 1; index < data.data.attributes.star; index++) {
+                setStar([...star,<FontAwesomeIcon icon={faStar} className="text-warning" />])
+                
+            }
+           
+            
         })
         .catch(err=>{
             console.log(err)
@@ -35,29 +44,30 @@ export default function SearchFilter() {
                     <h1>Search Filter</h1>
                     {
                         businesses.map((cv,idx,arr)=>{
-                           return  <Link key={idx} to="/detail">
+                           return  <Link key={idx} to={"/detail?hotel_id="+cv.id}>
                                         <Card  className="p-3 mb-4">
                                             <Row>
                                                 <Col sm={3}>
+                                                    {console.log('cv--->',cv)}
+                                                    <Card.Img className="img-fluid" variant="top" src={ (cv.attributes.photo.data !== null) ?URL+cv.attributes.photo.data[0].attributes.url :img} />
                                                     
-                                                    <Card.Img className="img-fluid" variant="top" src={URL+cv.attributes.photo.data[0].attributes.url} />
                                                 </Col>
                                                 <Col sm={9}>
                                                     <Card.Body>
                                                         <Card.Title>{cv.attributes.name}</Card.Title>
                                                         <Badge className="p-2 fs-4"  bg="success">3.9</Badge> 
                                                         <span>
-                                                                <FontAwesomeIcon icon={faStar} className="text-warning" />
-                                                                <FontAwesomeIcon icon={faStar} className="text-warning" />
-                                                                <FontAwesomeIcon icon={faStar} className="text-warning" />
-                                                                <FontAwesomeIcon icon={faStar} className="text-secondary" />
-                                                                <FontAwesomeIcon icon={faStar} className="text-secondary" />
+                                                                {
+                                                                    star.map((cv2,idx2,arr2)=>{
+                                                                        return cv2;
+                                                                    })
+                                                                }
                                                         </span>
                                                         <span>5,551 Rating</span>
                                                         <Card.Text>
                                                         {cv.attributes.desc}
                                                         </Card.Text>
-                                                        <a href={"tel:"+cv.attributes.phone} className="btn btn-success" >{'+91-'+cv.attributes.phone  }</a>
+                                                        <a href={"tel:"+cv.attributes.phone} className="btn btn-success" onClick={(e)=>{ e.stopPropagation();  }}>{'+91-'+cv.attributes.phone  }</a>
                                                     </Card.Body>
                                                 </Col>
                                             </Row>
